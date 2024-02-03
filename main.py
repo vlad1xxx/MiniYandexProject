457d3e027efaf7fedb8d57dbe2b1b3db1e9ab91c
vlad1xxx <vlad1x2807@gmail.com>
2024-02-03 16:36:06 +0300

    add chages

--- main.py
+++ main.py
@@ -6,22 +6,38 @@ import requests
 
 
 class MapApp(QMainWindow):
-    def __init__(self):
+    def __init__(self, lon, lat, delta, view='map'):
         super().__init__()
+        self.lon = lon
+        self.lat = lat
+        self.delta = delta
+        self.view = view
         uic.loadUi("main.ui", self)
         self.setWindowTitle("Yandex Map")
         self.update_map()
 
+    def get_image(self):
+        req_url = f"http://static-maps.yandex.ru/1.x/"
+        params = {
+            "ll": ",".join((self.lon, self.lat)),
+            "spn": ",".join((self.delta, self.delta)),
+            "l": self.view,
+            "size": '650,450'
+        }
+        response = requests.get(req_url, params=params)
+        return response.content
+
     def update_map(self):
-        response = requests.get("https://static-maps.yandex.ru/1.x/?ll=37.677751,55.757718&spn=0.016457,"
-                                "0.00619&l=map&size=650,450")
         pixmap = QPixmap()
-        pixmap.loadFromData(response.content)
+        pixmap.loadFromData(self.get_image())
         self.image.setPixmap(pixmap)
 
 
 if __name__ == "__main__":
+    lon = input()
+    lat = input()
+    delta = input()
     app = QApplication(sys.argv)
-    ex = MapApp()
+    ex = MapApp(lon, lat, delta)
     ex.show()
     sys.exit(app.exec_())
