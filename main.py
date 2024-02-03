cb39f3a1f5f5982af2807a54b91f1d7befe4d090
vlad1xxx <vlad1x2807@gmail.com>
2024-02-03 17:02:42 +0300

    added limit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
+
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPixmap
 import requests
@@ -19,8 +21,8 @@ class MapApp(QMainWindow):
     def get_image(self):
         req_url = f"http://static-maps.yandex.ru/1.x/"
         params = {
-            "ll": ",".join((self.lon, self.lat)),
-            "spn": ",".join((self.delta, self.delta)),
+            "ll": ",".join((str(self.lon), str(self.lat))),
+            "spn": ",".join((str(self.delta), str(self.delta))),
             "l": self.view,
             "size": '650,450'
         }
@@ -28,18 +30,43 @@ class MapApp(QMainWindow):
         if response.status_code == 200:
             return response.content
         print(f'Ошибка {response.status_code}')
+        self.close()
 
     def update_map(self):
         pixmap = QPixmap()
         pixmap.loadFromData(self.get_image())
         self.image.setPixmap(pixmap)
 
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Left:
+            if self.lon - self.delta < -179:
+                self.lon = -179
+            else:
+                self.lon -= self.delta
+        elif event.key() == Qt.Key_Right:
+            if self.lon - self.delta > 179:
+                self.lon = 179
+            else:
+                self.lon += self.delta
+        elif event.key() == Qt.Key_Up:
+            if self.lat + self.delta > 85:
+                self.lat = 85.0
+            else:
+                self.lat += self.delta
+        elif event.key() == Qt.Key_Down:
+            if self.lat - self.delta < -85:
+                self.lat = -85.0
+            else:
+                self.lat -= self.delta
+        self.update_map()
+
 
 if __name__ == "__main__":
-    lon = input()
-    lat = input()
-    delta = input()
+    lon = float(input())
+    lat = float(input())
+    delta = float(input())
     app = QApplication(sys.argv)
     ex = MapApp(lon, lat, delta)
     ex.show()
     sys.exit(app.exec_())
+    # 55.702647, 37.530834
