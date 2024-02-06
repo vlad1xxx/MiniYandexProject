9dd53c3abd3424bf21d1046859b1bc47f83c9f78
vlad1xxx <vlad1x2807@gmail.com>
2024-02-06 23:52:19 +0300

    update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class MapApp(QMainWindow):
     def initUI(self):
         self.search_btn.setIcon(QIcon('images/image.png'))
         self.search_btn.clicked.connect(self.search)
-        #кнопки смены режима карты
+        # кнопки смены режима карты
         self.scheme.clicked.connect(self.make_map)
         self.satellite.clicked.connect(self.make_satellite)
         self.hybrid.clicked.connect(self.make_hybrid)
@@ -86,19 +86,19 @@ class MapApp(QMainWindow):
         pixmap.loadFromData(self.get_image())
         self.image.setPixmap(pixmap)
 
-    #режим 'гибрид'
+    # режим 'гибрид'
     def make_hybrid(self):
         self.view = 'skl'
         self.update_map()
         self.setFocus()
 
-    #режим 'схема'
+    # режим 'схема'
     def make_map(self):
         self.view = 'map'
         self.update_map()
         self.setFocus()
 
-    #режим 'спутник'
+    # режим 'спутник'
     def make_satellite(self):
         self.view = 'sat'
         self.update_map()
@@ -127,7 +127,7 @@ class MapApp(QMainWindow):
                 self.lat = -85.0
             else:
                 self.lat -= self.delta
-        #изменение масштаба
+        # изменение масштаба
         elif event.key() == Qt.Key_PageUp:
             if self.delta > 0.001:
                 self.delta /= 2.4
@@ -142,4 +142,3 @@ if __name__ == "__main__":
     ex = MapApp()
     ex.show()
     sys.exit(app.exec_())
-
